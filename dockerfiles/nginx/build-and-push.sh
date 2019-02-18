66347b930b5280352677873070e632cb9157d5bd
Pietro Bongiovanni <pietro@fixico.nl>
2019-02-18 09:37:45 +0100

    Initial commit :bomb:

---
+++ dockerfiles/nginx/build-and-push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+# Repository to push images to
+REPOSITORY=pgoodjohn
+
+# Name for the images
+NAME=nginx
+
+# For every dockerfile we create and push the image
+# VERSION=$(echo $1 | sed -e "s/Dockerfile-//g")
+
+echo "Building $1 to $REPOSITORY/$NAME:$VERSION"
+docker build -q -f ./$1 -t $REPOSITORY/$NAME:$VERSION .
+
+docker push $REPOSITORY/$NAME:$VERSION
